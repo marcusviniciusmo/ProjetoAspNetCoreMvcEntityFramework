608db8ea1b66ee934beb42b0488ce75a0cadd6bb
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-11-10 21:48:46 +0100

    Tela de detalhes e eager loading

--- ProjetoSalesWebMvc/ProjetoSalesWebMvc/Services/SellerService.cs
+++ ProjetoSalesWebMvc/ProjetoSalesWebMvc/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using ProjetoSalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjetoSalesWebMvc.Services
 {
@@ -27,7 +28,7 @@ namespace ProjetoSalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(s => s.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(s => s.Id == id);
         }
 
         public void Remove(int id)
