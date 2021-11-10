2a5e4084fb125877af2253d14fc0688c857fa418
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-11-10 10:56:23 +0100

    Outras entidades e segunda migration

--- ProjetoSalesWebMvc/ProjetoSalesWebMvc/Data/ProjetoSalesWebMvcContext.cs
+++ ProjetoSalesWebMvc/ProjetoSalesWebMvc/Data/ProjetoSalesWebMvcContext.cs
@@ -14,6 +14,8 @@ namespace ProjetoSalesWebMvc.Data
         {
         }
 
-        public DbSet<ProjetoSalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
