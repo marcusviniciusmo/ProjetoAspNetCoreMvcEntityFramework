638a46b965a3bf9151f85319c97d3aee644c71c5
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2021-11-11 21:08:42 +0100

    Últimos ajustes e finalização do projeto

--- ProjetoSalesWebMvc/ProjetoSalesWebMvc/Models/SalesRecord.cs
+++ ProjetoSalesWebMvc/ProjetoSalesWebMvc/Models/SalesRecord.cs
@@ -11,7 +11,7 @@ namespace ProjetoSalesWebMvc.Models
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:F2}")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
